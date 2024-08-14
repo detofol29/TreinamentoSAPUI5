ee7f2d1fe52041c636ceb74f78d3dc198e4763a7
Gabriel Detofol <detofol29@gmail.com>
2024-08-14 15:12:14 -0300

    P15

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,17 +4,5 @@ sap.ui.define([
 ], (Controller, MessageToast) => {
     "use strict";
 
-    return Controller.extend("ui5.walkthrough.controller.App", {
-        onShowHello(){
-
-            //Obtém o ResourceBundle associado ao modelo "i18n", permitindo o acesso às mensagens traduzidas
-            const oPacoteInter = this.getView().getModel("i18n").getResourceBundle();
-
-            //Obtém o valor da propriedade /recipient/name
-            const sRecipiente = this.getView().getModel().getProperty("/recipient/name");
-
-            const sMsg = oPacoteInter.getText("BomDiaMsg"," " + [sRecipiente]);
-            MessageToast.show(sMsg);
-        }
-    });
+    return Controller.extend("ui5.walkthrough.controller.App", {});
 });
---
+++ webapp/controller/BomDiaPanel.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], (Controller, MessageToast) => {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.BomDiaPanel", {
+        onShowHello(){
+
+            //Obtém o ResourceBundle associado ao modelo "i18n", permitindo o acesso às mensagens traduzidas
+            const oPacoteInter = this.getView().getModel("i18n").getResourceBundle();
+
+            //Obtém o valor da propriedade /recipient/name
+            const sRecipiente = this.getView().getModel().getProperty("/recipient/name");
+
+            const sMsg = oPacoteInter.getText("BomDiaMsg"," " + [sRecipiente]);
+            MessageToast.show(sMsg);
+        }
+    });
+});
